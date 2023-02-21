79da4fda839a00a20caec8bebc1759da64de043d
Johnathan Booy <106690990+johnathan-booy@users.noreply.github.com>
2023-02-20 20:57:22 -0800

    bubbleSort

--- bubble.js
+++ bubble.js
@@ -1,3 +1,17 @@
-function bubbleSort(arr) {}
+function bubbleSort(arr) {
+	for (let i = 0; i < arr.length; i++) {
+		let switched = false;
+		for (let j = 0; j < arr.length - i; j++) {
+			if (arr[j] > arr[j + 1]) {
+				const temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+				switched = true;
+			}
+		}
+		if (!switched) break;
+	}
+	return arr;
+}
 
 module.exports = bubbleSort;
